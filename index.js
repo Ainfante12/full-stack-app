c4a3ac57aa510b79f1cc082f738e496f07a1f2eb
SHAM <shamarknibbs@gmail.com>
2020-01-19 00:17:01 -0800

    added sequlize - SK

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 5432;
+let Sequelize= require('sequelize');
+let sequelize= new Sequelize('postgres://postgres:Kingsmar24@localhost:5432/postgres');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
